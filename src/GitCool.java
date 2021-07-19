c374accf6ac405bf75b4aed17e08e5d2e60dbb14
jamina17 <jabdinbekova@hotmail.com>
2021-07-19 15:44:56 -0500

    added comments

--- src/GitCool.java
+++ src/GitCool.java
@@ -3,4 +3,7 @@ public class GitCool {
     //B23 is learning git
 
     //We have added out github account under intellij
+
+    //we were able to successfully share project under out github account
+    //share project on github happen only one time per project
 }
